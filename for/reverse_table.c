1b212f3a75ccbfd7a5056fb5a89773b6e24c27b9
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-08-11 15:31:15 +0200

    with symbolic constants

--- for/reverse_table.c
+++ for/reverse_table.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+#define  SUCCESS  0
+#define  LOWER    0
+#define  UPPER    300
+#define  STEP     -20
+
 int main() {
   float fahr;
-    printf("°F  |  °C\n");
-  for (fahr = 300; fahr >= 0; fahr -= 20) {
+
+  printf("°F  |  °C\n");
+
+  for (fahr = UPPER; fahr >= LOWER; fahr += STEP) {
     printf("%3.0f | %4.2f \n", fahr, (fahr-32)*5/9);
   }
-  return 0;
+  return SUCCESS;
 }
